b2d4c249aa116f11fc28bf898a3682fb5ff2eb74
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2020-12-29 10:47:22 -0500

    First Commit

---
+++ src/app/validators/teacher.js
@@ -0,0 +1,29 @@
+
+function checkAllfields(body){
+
+    const keys = Object.keys(body);
+    for(let key of keys){
+        if(body[key]==""){
+          const error = "Please, fill out all fiels!"
+          return { 
+              error: error,
+              body
+        }
+    }
+}
+}
+
+
+ function post(req, res, next){ 
+
+    const fieldsInBlank = checkAllfields(req.body)
+    if(fieldsInBlank) return res.send("Fill all lines")
+    
+    next()
+    }
+
+
+module.exports = { 
+
+    post
+}
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require("./app/controllers/teachers")
+const TeacherValidator = require('./app/validators/teacher')
+const students = require("./app/controllers/students")
+
+routes.get('/', function (req, res){
+
+    return res.redirect("/teachers")
+
+})
+
+routes.get('/teachers', teachers.index)
+routes.get("/teachers/create", teachers.create)
+routes.post("/teachers", TeacherValidator.post, teachers.post)
+routes.get("/teachers/:id", teachers.show)
+routes.get("/teachers/:id/edit", teachers.edit )
+routes.put("/teachers", teachers.update) //put
+routes.delete("/teachers", teachers.delete) // working on it
+
+
+routes.get('/students', students.index)
+routes.get("/students/create", students.create)
+routes.post("/students", students.post)
+routes.get("/students/:id", students.show)
+routes.get("/students/:id/edit", students.edit )
+routes.put("/students", students.update) //put
+routes.delete("/students", students.delete) // working on it
+
+module.exports = routes
